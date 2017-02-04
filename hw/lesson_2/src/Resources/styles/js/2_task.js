084af67bc57b56d41f8470652e3cd6493cd61449
AnnaMolybog <anna.molybog@gmail.com>
2017-02-04 22:57:29 +0200

    Lesson-2: task 2

--- hw/lesson_2/src/Resources/styles/js/2_task.js
+++ hw/lesson_2/src/Resources/styles/js/2_task.js
@@ -1,6 +1,18 @@
 window.onload = handleAge;
 
+const MIN_ALLOWED_AGE = 18;
+
 function handleAge() {
-    var age = prompt('Enter your age please');
-    
+    var age = parseInt(prompt('Enter your age please'));
+
+    if (typeof(age)  == "number" && age > 0) {
+        if (age < MIN_ALLOWED_AGE) {
+            document.write('Alcohol drinks are not allowed');
+        } else {
+            document.write('You can legally drink alcohol');
+        }
+    } else {
+        document.write('Invalid input value');
+    }
+
 }
